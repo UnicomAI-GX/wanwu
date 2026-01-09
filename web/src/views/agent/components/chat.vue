01e0dfbfa20579fa31fb9b9acabcbb4179e6a915
sofia01 <17701216705@163.com>
2026-01-09 18:22:36 +0800

    fix(web): 解决暂停打印按钮状态改变问题

--- web/src/views/agent/components/chat.vue
+++ web/src/views/agent/components/chat.vue
@@ -25,7 +25,6 @@
         <!--停止生成-重新生成-->
         <div class="center-editable">
           <div v-show="stopBtShow" class="stop-box">
-            {{ sessionStatus }}
             <span v-show="sessionStatus === 0" class="stop" @click="preStop">
               <img
                 class="stop-icon mdl"
